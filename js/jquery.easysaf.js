e494f7a4ebcb001c741047e5a15dc309ac8fd736
Damian Dabrowski <damian@Damians-MacBook-Pro.local>
2016-11-30 04:35:46 +0100

    Option to count displayed elements after filtering and sorting

--- js/jquery.easysaf.js
+++ js/jquery.easysaf.js
@@ -13,12 +13,21 @@ $.EasySAF = function() {
             item: '.box-item',
 
             activeFilters: '',
-            activeFiltersHolder: '.active-filters'
+            activeFiltersHolder: '.active-filters',
+
+            displayNumberOfMatchedElements: false,
+            matchedElementsHolder: '.matched-elements',
+            searchBoxResultDelay: 500
         }
-    })
+    });
     
     var saf = self.selectors;
     
+    if (saf.displayNumberOfMatchedElements) {
+        var timeToShow = null;
+         checkNumberOfMatchedItems();
+    }
+
     $(saf.filterClass).click(function() {
         if ( saf.activeFilters.indexOf(this.id) == -1 ) {
             $(this).prepend(' <i class="fa fa-check"> </i> ')
@@ -28,15 +37,28 @@ $.EasySAF = function() {
             $(this).children('i').remove();
             $(saf.activeFiltersHolder + ' .' + this.id).remove();
             saf.activeFilters = saf.activeFilters.replace('.' + this.id, '');
-            $(saf.item + saf.activeFilters).show();
+
+            if (saf.activeFilters === '') {
+                $(saf.item).show();
+            }
         }
         
-        $(saf.item).hide();
-        $(saf.item + saf.activeFilters).show();
+        if ( saf.activeFilters != '' ) {
+            $(saf.item).hide();
+            $(saf.item + saf.activeFilters).show();
+        }
+
+        if ( saf.displayNumberOfMatchedElements ) {
+             checkNumberOfMatchedItems();
+        }
     });
     
     $(saf.filterCleaner).click(function() {
         cleanUpFilters();
+
+        if ( saf.displayNumberOfMatchedElements ) {
+             checkNumberOfMatchedItems();
+        }
     });
     
     
@@ -61,20 +83,55 @@ $.EasySAF = function() {
             cleanUpFilters();
         } else {
             $(saf.item).hide();
-            
-            $(saf.item + saf.activeFilters).each(function() {
-                if ( $(this).attr('data-name').toLowerCase().indexOf(value) >= 0 ) {
-                    $(this).show().css({ 'opacity' : 0 }).animate( { 'opacity' : 1 }, 500 );
-                }
-            });
         }
+
+        checkIfUsingSearchBox(value);
     });
     
+
+
+    function checkNumberOfMatchedItems() {
+        var activeItems = 0;
+
+        $(saf.item).filter(function() {
+            if ( $(this).css('display') != 'none' ) {
+                activeItems += 1;
+            }
+        });
+
+        $(saf.matchedElementsHolder).text(activeItems);
+    }
+
+    function checkIfUsingSearchBox(value) {
+        clearTimeout(timeToShow);
+        timeToShow = setTimeout(function() {
+
+            if ( saf.displayNumberOfMatchedElements === true ) {
+                var matchedElements = 0;
+
+                $(saf.item + saf.activeFilters).each(function() {
+                    if ( $(this).attr('data-name').toLowerCase().indexOf(value) >= 0 ) {
+                        $(this).show().css({ 'opacity' : 0 }).animate( { 'opacity' : 1 }, 500 );
+                        matchedElements += 1;
+                    }
+                });
+
+                $(saf.matchedElementsHolder).text(matchedElements);
+            } else {
+                $(saf.item + saf.activeFilters).each(function() {
+                    if ( $(this).attr('data-name').toLowerCase().indexOf(value) >= 0 ) {
+                        $(this).show().css({ 'opacity' : 0 }).animate( { 'opacity' : 1 }, 500 );
+                    }
+                });
+            }
+        }, saf.searchBoxResultDelay);
+    }
+
     function cleanUpFilters() {
         saf.activeFilters = '';
-        $(saf.item).show();
         $(saf.searchBar).val('');
         $(saf.activeFiltersHolder).empty();
         $('i.fa-check').remove();
+        $(saf.item).show();
     }
 }
